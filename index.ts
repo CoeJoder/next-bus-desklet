5b0d313266bfc30d15f3cf8c9c3f7d78df36328b
CoeJoder <CoeJoder@users.noreply.github.com>
2025-07-27 10:30:49 -0700

    Implement stop query

--- index.ts
+++ index.ts
@@ -4,9 +4,26 @@ import util from 'node:util';
 import OnebusawaySDK from 'onebusaway-sdk';
 import fetch from 'node-fetch';
 
-type OBAListResponse = 
+interface MyStop {
+  id: string,
+  name: string,
+  code?: string,
+  direction?: string,
+  group?: string, 
+}
+
+type OBAStopGroup = OnebusawaySDK.StopsForRouteListResponse.Data.Entry.StopGrouping;
+
+interface OBAStopGrouping {
+  stopGroups?: OBAStopGroup[],
+}
+
+type OBAListResponseV1 = 
   | OnebusawaySDK.RoutesForAgency.RoutesForAgencyListResponse
   | OnebusawaySDK.AgenciesWithCoverage.AgenciesWithCoverageListResponse;
+
+type OBAListResponseV2 = 
+  | OnebusawaySDK.StopsForRoute.StopsForRouteListResponse;
 
 const inspect = (obj: unknown) => {
   return util.inspect(obj, {
@@ -14,7 +31,7 @@ const inspect = (obj: unknown) => {
     depth: 10,
     maxArrayLength: 10,
     sorted: true,
-    breakLength: 120,
+    breakLength: 140,
   })
 };
 
@@ -22,10 +39,16 @@ const generateLogFilename = () => {
   const date = new Date();
   const timestamp = date.toISOString().replace(/:/g, '-').replace(/\./g, '_');
   return `./logs/log-${timestamp}.txt`;
-}
+};
 
-const throwIfError = (response: OBAListResponse, msg = "Server error") => {
+const throwIfErrorV1 = (response: OBAListResponseV1, msg = "Server error") => {
   if (!response.data || !response.data.list || !response.data.references || response.data.limitExceeded) {
+    throw new Error(msg);
+  }
+};
+
+const throwIfErrorV2 = (response: OBAListResponseV2, msg = "Server error") => {
+  if (!response.data || !response.data.references) {
     throw new Error(msg);
   }
 };
@@ -39,8 +62,7 @@ const env = (varName: string): string => {
 // we are interested in just one particular agency route stop, both directions
 const AGENCY_NAME = env('AGENCY_NAME');
 const ROUTE_SHORTNAME = env('ROUTE_SHORTNAME');
-const STOP_ID_EAST = env('STOP_ID_EAST');
-const STOP_ID_WEST = env('STOP_ID_WEST');
+const STOP_NAME = env('STOP_NAME');
 
 // OneBusAway API endpoint
 const OBA_API_KEY = env('OBA_API_KEY');
@@ -69,9 +91,14 @@ const client = new OnebusawaySDK({
   },
 });
 
+// 0. Find OBA version info
+// const config = await client.config.retrieve();
+// console.log("Config:", inspect(config.data.entry));
+// process.exit();
+
 // 1. Find agency by name
 const agencies = await client.agenciesWithCoverage.list();
-throwIfError(agencies)
+throwIfErrorV1(agencies)
 const agency = agencies.data.references.agencies
   .find(agency => agency.name == AGENCY_NAME);
 console.log('Target agency:', inspect(agency));
@@ -79,13 +106,51 @@ if (!agency) throw new Error("Agency not found");
 
 // 2. Find route by short-name within the target agency
 const routes = await client.routesForAgency.list(agency.id);
-throwIfError(routes);
+throwIfErrorV1(routes);
 const route = routes.data.list
   .find(route => route.shortName == ROUTE_SHORTNAME);
 console.log("Target route:", inspect(route));
 if (!route) throw new Error("Route not found");
 
-// 3. Call `arrivals-and-departures-for-stop` and find the `tripId` for my stop, going EAST and WEST
+// 3. Find stop (both groups/directions) by name, route, and day (today)
+const stopsForRoute = await client.stopsForRoute.list(route.id, {
+  includePolylines: false,
+  time: Date.now().toString(),
+});
+throwIfErrorV2(stopsForRoute);
+const stopRefs = stopsForRoute.data.references.stops
+  .filter(stop => stop.name == STOP_NAME);
+
+// The TypeScript client types don't accurately describe the actual response data.
+// They also mismatches the docs: https://developer.onebusaway.org/api/where/methods/stops-for-route
+// Could be bug or version discrepancy.
+// For now, we'll override with casting of our own type aliases.
+
+// add group/direction info to the targets by cross-referencing their stopIds
+let stops: MyStop[] = [];
+// @ts-expect-error
+stopsForRoute.data.entry.stopGroupings?.forEach((stopGrouping: OBAStopGrouping) => {
+  stopGrouping.stopGroups?.forEach(stopGroup => {
+    stopGroup.stopIds?.forEach(stopId => {
+      stopRefs.forEach(stopRef => {
+        if (stopRef.id == stopId) {
+          stops.push({
+            id: stopRef.id,
+            name: stopRef.name,
+            code: stopRef.code,
+            direction: stopRef.direction,
+            group: stopGroup.name?.name,
+          });
+        }
+      });
+    });
+  });
+});
+
+console.log("Target stops:", inspect(stops));
+
+// 4. Call `arrivals-and-departures-for-stop` and find the `tripId` for target stops, in both directions/groups
+// const arrivalsAndDepartures = await client.arrivalAndDeparture.list()
 // const arrsAndDepsWest = await client.arrivalAndDeparture.list(STOP_ID_WEST).retrieve();
 // console.log(arrsAndDepsWest);
 
